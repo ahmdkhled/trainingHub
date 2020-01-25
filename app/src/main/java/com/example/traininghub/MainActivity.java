6426ae1b4ecfd0e0b5b16e50bc6d413fd2fcbfc3
ahmdkhled <ahmdkhled@gmail.com>
2020-01-25 23:10:06 +0200

    add bottomNavigationView to mainActivity

--- app/src/main/java/com/example/traininghub/MainActivity.java
+++ app/src/main/java/com/example/traininghub/MainActivity.java
@@ -12,21 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.fragment_main);
+        setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView=findViewById(R.id.coursesForYouRecycler);
-        RecyclerView categoriesRecycler=findViewById(R.id.categories_recycler);
-        RecyclerView recentlyAdded_recycler=findViewById(R.id.recentlyAdded_recycler);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false));
-        CoursesAdapter coursesAdapter=new CoursesAdapter(this);
-        recyclerView.setAdapter(coursesAdapter);
-
-        recentlyAdded_recycler.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false));
-        recentlyAdded_recycler.setAdapter(coursesAdapter);
-
-        categoriesRecycler.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false));
-        CategoriesAdapter categoriesAdapter=new CategoriesAdapter(this);
-        categoriesRecycler.setAdapter(categoriesAdapter);
+//        RecyclerView recyclerView=findViewById(R.id.coursesForYouRecycler);
+//        RecyclerView categoriesRecycler=findViewById(R.id.categories_recycler);
+//        RecyclerView recentlyAdded_recycler=findViewById(R.id.recentlyAdded_recycler);
+//
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false));
+//        CoursesAdapter coursesAdapter=new CoursesAdapter(this);
+//        recyclerView.setAdapter(coursesAdapter);
+//
+//        recentlyAdded_recycler.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false));
+//        recentlyAdded_recycler.setAdapter(coursesAdapter);
+//
+//        categoriesRecycler.setLayoutManager(new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false));
+//        CategoriesAdapter categoriesAdapter=new CategoriesAdapter(this);
+//        categoriesRecycler.setAdapter(categoriesAdapter);
     }
 }
