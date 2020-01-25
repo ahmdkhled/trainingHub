28addfd083f237e339960ea1395075597f52b984
ahmdkhled <ahmdkhled@gmail.com>
2020-01-25 05:20:23 +0200

    design courses for you section in main fragment

---
+++ app/src/main/java/com/example/traininghub/CoursesAdapter.java
@@ -0,0 +1,44 @@
+package com.example.traininghub;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CoursesAdapter extends RecyclerView.Adapter<CoursesAdapter.CourseHolder> {
+
+    private Context context;
+
+    public CoursesAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CourseHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view=LayoutInflater.from(context).inflate(R.layout.layout_course,parent,false);
+        return new CourseHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    class CourseHolder extends RecyclerView.ViewHolder{
+
+        public CourseHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
