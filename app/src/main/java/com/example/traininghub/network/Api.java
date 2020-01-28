d130130cb227c18e83dd357bf4c1ed21294d1c29
ahmdkhled <ahmdkhled@gmail.com>
2020-01-29 01:47:46 +0200

    feat:implement retrofit client

---
+++ app/src/main/java/com/example/traininghub/network/Api.java
@@ -0,0 +1,4 @@
+package com.example.traininghub.network;
+
+public interface Api {
+}
---
+++ app/src/main/java/com/example/traininghub/network/RetrofitClient.java
@@ -0,0 +1,28 @@
+package com.example.traininghub.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    
+    private static  RetrofitClient retrofitClient;
+    private Retrofit retrofit;
+    private Api api;
+
+    public static RetrofitClient getInstance() {
+        return retrofitClient == null ? retrofitClient=new RetrofitClient() : retrofitClient;
+    }
+
+    private Retrofit getRetrofit()
+    {
+        return retrofit==null?new Retrofit.Builder()
+                .baseUrl("http://traininghub.tk")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build():retrofit;
+
+    }
+
+    public Api getApi(){
+        return api==null?api=getRetrofit().create(Api.class):api;
+    }
+}
